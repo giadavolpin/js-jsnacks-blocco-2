0d79b1fb4b3ae418e6a87fbe20d5568d51a84915
giadavolpin <volpingiada@gmail.com>
2022-10-23 16:38:19 +0200

    3

--- script3.js
+++ script3.js
@@ -4,21 +4,29 @@ che sono in posizione dispari.
 
 
 1. Crea un array di numeri interi 
-2. prendere tutti i numeri dispari
-3. fai la somma di tutti gli elementi che sono in posizione(indice) dispari
+2. prendere tutti i numeri dispari e metterli dentro ad un array 
+3. sommare un array
 */
 
-const i = [10, 5, 50, 2, 30, 24, 15, 75];
-
+const numeri = [10, 5, 50, 2, 30, 24, 15, 75];
+let sommaDispari = []
 
 for (let i = 1; i < 10; i++){
    
     if (i % 3 == 0) {
-      Dispari = i;
-
+      dispari = i;
     }
   }
-  
-  console.log(sommaDispari);
+    numeri=[];  
+    
+    for(let i = 1; i<numeri.length; i++){
+        numeri += numeri[i];
+        i++;
+    }
+    let text = numeri.toString();
+    
+    numeridispari.innerHTML = 'La somme degli indici dispari è: ' + i;
+    
+    console.log(sommaDispari);
 
  /* DA FARE IL 3 XK NON VIENE */ 
